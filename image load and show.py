87b8b0f506f512a404f40ad91bc55ff6cbbcb229
Ben-Aff <r770529885@163.com>
2023-03-07 00:01:17 +0800

    forth commit at 3/6

--- image load and show.py
+++ image load and show.py
@@ -88,7 +88,7 @@ def math2_demo():
     cv.imshow('result1',result1)
     cv.imshow('result2',result2)
     cv.waitKey(0)
-    cv.destroyAllWindows(0)
+    cv.destroyAllWindows()
 #opencv中的键盘相应操作
 def key_demo():
     pass
@@ -130,7 +130,71 @@ def color_table_demo():
         if c==27:
             break
     cv.destroyAllWindows()
+#opencv中图像像素的逻辑操作
+def beiwise_demo():
+    image = cv.imread('E:/8.opnecv4/opencv/images/test.png')
+    b1=np.zeros((400,400,3),dtype=np.uint8)
+    b1[:,:]=(255,0,255)
+    b2=np.zeros((400,400,3),dtype=np.uint8)
+    b2[:,:]=(0,255,0)
+    dst1=cv.bitwise_and(b1,b2)
+    dst2=cv.bitwise_or(b1,b2)
+    cv.imshow('b1',b1)
+    cv.imshow('b2',b2)
+    cv.imshow('bitwise_and',dst1)
+    cv.imshow('bitwise_or',dst2)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+#opencv中通道的分离与合并
+def channel_split():
+#使用python中的切片进行通道的分离与合并
+    b1=cv.imread('E:/8.opnecv4/opencv/images/yuan_test.png')
+    cv.imshow('input',b1)
+    cv.imshow('b1',b1[:,:,2])
+    mv=cv.split(b1)
+    mv[0][:,:]=255
+    result=cv.merge(mv)
+    cv.imshow('result',result)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+#opencv中图像色彩空间转换
+def color_space_demo():
+    b1=cv.imread('E:/8.opnecv4/opencv/images/greenback.png')
+    print(b1.shape)
+    cv.imshow('input',b1)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    hsv=cv.cvtColor(b1,cv.COLOR_BGR2HSV)
+    cv.imshow('hsv',hsv)
+    mask=cv.inRange(hsv,(35,43,36),(77,255,255))
+    cv.bitwise_not(mask,mask)
+    result=cv.bitwise_and(b1,b1,mask=mask)
+    cv.imshow('mask',hsv)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+#opencv中像素值统计
+def pixel_sta():
+    b1=cv.imread('E:/8.opnecv4/opencv/images/ttt.png')
+    print(b1.shape)
+    cv.imshow('input',b1)
+    means,dev=cv.meanStdDev(b1)
+    print(means,dev)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    b2=np.zeros((512,512,3),dtype=np.uint8)
+    print(b1.shape)
+    means,dev=cv.meanStdDev(b2)
+    print(means,dev)
+    b3=np.zeros((512,512,3),dtype=np.uint8)
+    print(np.min(b1[:,:,-1]))#统计通道的最小值
+    print(np.max(b1[:,:,-1]))#统计通道的最大值
+
+
+
+
+
+
 
 
 if __name__=='__main__':
-    color_table_demo()
+    pixel_sta()
