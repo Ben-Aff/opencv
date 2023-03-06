a545b83452e3e96b4ab670fe629ce360062d0248
renweilun <renweilun@nimte.ac.cn>
2023-03-06 11:57:35 +0800

    second commit at 3/6

--- image load and show.py
+++ image load and show.py
@@ -7,7 +7,21 @@ def read_demo():
     cv.imshow('input test image-LINNA',image)
     cv.waitKey(0)
     cv.destroyAllWindows
-#图片的色彩
+#图片的色彩空间转换
 def color_space_demo():
-    image=cv.imread
+    image=cv.imread('D:/project/Opencv/study2/images/test.png')
+    gray=cv.cvtColor(image,cv.COLOR_BGR2GRAY)#BGR TO GRAY
+    hsv=cv.cvtColor(image,cv.COLOR_BGR2HSV)#BGR TO HSV
+    cv.imshow('GRAY', gray)
+    cv.imshow('HSV', hsv)
+    cv.waitKey(0)
+    cv.destroyAllWindows
 
+
+
+
+
+
+
+if __name__=='__main__':
+    color_space_demo()
